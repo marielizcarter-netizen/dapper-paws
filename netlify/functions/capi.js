99d6d899c7281566ac28f44a8f71587078221ab1
marielizcarter-netizen <marielizcarter@gmail.com>
2026-09-03 13:25:22 -0700

    Update capi.js

--- netlify/functions/capi.js
+++ netlify/functions/capi.js
@@ -1,3 +1,21 @@
+async function getAccessToken() {
+  const credentials = Buffer.from(
+    `${process.env.REDDIT_CLIENT_ID}:${process.env.REDDIT_CLIENT_SECRET}`
+  ).toString('base64');
+
+  const response = await fetch('https://www.reddit.com/api/v1/access_token', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Basic ${credentials}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method not allowed' };
@@ -5,6 +23,9 @@ exports.handler = async (event) => {
 
   const data = JSON.parse(event.body);
   console.log('Received event:', data);
+
+  const token = await getAccessToken();
+  console.log('Got token:', token);
 
   return {
     statusCode: 200,
